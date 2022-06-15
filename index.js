d662b21c1170a6ce38d1df283642c19c39888d9b
Wangari-Sharon <wangarik35@gmail.com>
2022-06-15 15:57:34 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+console.log('Hello');
+
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then(res => res.json())
+    .then(obj => document.body.append(obj['id']))
+    .catch(err => document.body.append(err))
+
+}
